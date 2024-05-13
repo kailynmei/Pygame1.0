ca042c8cb9fe2dc6e1c7030c62f5a4d04ea176fe
Helena-Mansour <168466850+Helena-Mansour@users.noreply.github.com>
2024-05-13 18:18:40 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -30,11 +30,6 @@ def menu(tela,img1,img2):
                     menu = 'sumiu'
 
 
-pygame.mixer.music.set_volume(0)
-ativa_music_menu.play()
-menu(tela,)
 
-menu1 = pygame.image.load('Assets/Imagens/Wizard/branco.png').convert_alpha()
-menu2 = pygame.image.load('Assets/Imagens/Feiticeiro2/preto.png').convert_alpha()
 
     
